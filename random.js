60bde65023e03bfb4f72e3d9acfeda98a3d52da2
Artur Comoli <artur.comoli@gmail.com>
2021-10-27 16:55:21 -0300

    Otimização do código para geração de matrizes com valores aleatórios

--- random.js
+++ random.js
@@ -1,22 +1,16 @@
 function random(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function matrizRandom(n){
-  let mat = [];
-  for (let i = 0; i < n; i++) {
-      mat[i] = new Array(n);
-      for(let j = 0; j < n; j++){
-        mat[j] = new Array(n);
-        for (let i = 0; i < mat.length; i++){
-            for(let j = 0; j < mat.length; j++){
-                k = random(-100, 100)
-                mat[i][j] = k;
-            }
-        }
-      }
-  }
-  console.table(mat);
+let mat = [];
+for (let i = 0; i < n; i++) {
+    mat[i] = new Array(n);
+    for(let j = 0; j < n; j++){
+      mat [i][j] = random(-100, 100);        
+    }
+}
+console.table(mat);
 }
